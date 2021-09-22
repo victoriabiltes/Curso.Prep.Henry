36fc5c79faede40955f12928d31b5599578252f1
Mary <Mary@MacBook-Pro-de-Juan.local>
2021-09-21 23:18:27 -0300

    Actualizacion homework antes de esPrimo, homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -107,6 +107,12 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero <50 && numero >20){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function esEntero(numero) {
@@ -117,6 +123,12 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (Number.isInteger(numero)){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -124,6 +136,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (Number.isInteger(numero/3) && Number.isInteger(numero/5)){
+    return 'fizzbuzz';
+  }
+  else if (Number.isInteger(numero/3)){
+    return 'fizz';
+  }
+  else if (Number.isInteger(numero/5)){
+    return 'buzz';
+  }
+  else{
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -133,6 +157,21 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1===0 || num2===2 || num3===0){
+    return 'Error';
+  }
+  else if (num1<0 || num2<0 || num3<0){
+    return 'Hay negativos';
+  }
+  else if (num1>num2 && num1>num3){
+    return 'Número 1 es mayor y positivo';
+  }
+  else if (num3>num1 && num3>num2){
+    return num3+1;
+  }
+  else{
+    return false;
+  }
 }
 
 function esPrimo(numero) {
