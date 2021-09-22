dddd802bfd71bb2d34d2abc41e41c24ff21a804d
Mary <Mary@MacBook-Pro-de-Juan.local>
2021-09-22 12:51:23 -0300

    Homework finalizada, homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -214,11 +214,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for (i=0; i<=10; i++){
-    var tabla = [];
+  var tabla = [0];
+  for (i=1; i<=10; i++){
     tabla.push(i*6);
   }
-  console.log(tabla);
+  return(tabla);
 }
 
 function tieneTresDigitos(numero){
